7a0cb0743be58bfc269c1f319e808b9508d377f4
Dimitar Anguelov <d.angelov9090@gmail.com>
2023-05-10 00:55:12 +0300

    made the background

--- FlappyBird1.2/Assets/Scripts/Player.cs
+++ FlappyBird1.2/Assets/Scripts/Player.cs
@@ -2,11 +2,27 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private SpriteRenderer spriteRenderer;
+
+    public Sprite[] sprites;
+
+    private int spriteIndex;
+
     private Vector3 direction;
 
     public float gravity = -9.8f;
 
     public float strength = 5f;
+
+    private void Awake()
+    {
+       spriteRenderer = GetComponent<SpriteRenderer>(); 
+    }
+
+    private void Start()
+    {
+        InvokeRepeating(nameof(AnimateSpite), 0.15f, 0.15f);
+    }
 
     private void Update()
     {
@@ -25,5 +41,17 @@ public class Player : MonoBehaviour
 
         direction.y += gravity * Time.deltaTime;
         transform.position += direction * Time.deltaTime;
+    }
+
+    private void AnimateSpite()
+    {
+        spriteIndex++;
+
+        if (spriteIndex >= sprites.Length)
+        {
+            spriteIndex = 0;
+        }
+
+        spriteRenderer.sprite = sprites[spriteIndex];
     }
 }
